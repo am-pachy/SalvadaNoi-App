eeca3d0830e10643b2a57c82604f541de6898dc8
am-pachy <anna.marchetto.vi@gmail.com>
2026-03-17 12:50:29 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://zjqgwkttdnvkfxnjumzg.supabase.co';
-const supabaseAnonKey = 'REPLACE_WITH_YOUR_SUPABASE_ANON_KEY';
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqcWd3a3R0ZG52a2Z4bmp1bXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNjQxNTUsImV4cCI6MjA4ODg0MDE1NX0.OfcI5HMSBTtDCKakCzqNtdOHx8ik8YD3om76-E0X_os";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
